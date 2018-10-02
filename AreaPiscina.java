c68a7bc9715a457d77645caff17e7af1d826bf41
ElianMelo <elianmelomorais@gmail.com>
2018-10-02 17:22:00 -0300

    Métodos e Parâmetros

--- AreaPiscina.java
+++ AreaPiscina.java
@@ -1,14 +1,12 @@
 class AreaPiscina {
   public static void main(String[] args){
-    //raio da piscina
     double raio = 2;
-    //área da piscina
     double areap;
-    //valor do pi
     final double pi = 3.14159;
 
-    double areap = pi * raio * raio;
-
+    areap = pi * raio * raio;
+    //areap = Math.PI * Math.pow(raio, 2);
     System.out.println("área: " + areap);
   }
+
 }
