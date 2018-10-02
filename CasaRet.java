0e07c85fa47a8f925932274701ba715871e8031d
ElianMelo <elianmelomorais@gmail.com>
2018-10-02 17:17:58 -0300

    Operações Aritméticas e Variáveis

--- CasaRet.java
+++ CasaRet.java
@@ -4,6 +4,28 @@
 */
 class CasaRet {
   public static void main(String[] args){
-    System.out.println("Programa para cálculo \nda área da casa");
+    //Primeira Etapa
+
+    /*
+    System.out.println("Programa para cálculo da área da casa");
+    System.out.println("A área da sala é " + 10*10);
+    System.out.println("A área do quarto é " + 5*7);
+    System.out.println("A área do banheiro é " + 5*7);
+    System.out.println("A área total é " + (10*10 + 5*7 + 5*7));
+    */
+
+    //Segunda Etapa
+    int areaq; //área do quarto
+    int areas; //área da sala
+    int areat; //área total
+
+    System.out.println("Programa para cálculo da área da casa");
+    areas = 10*10;
+    System.out.println("A área da sala é " + areas);
+    areaq = 7*5;
+    System.out.println("A área do quarto é " + areaq);
+    System.out.println("A área do banheiro é " + areaq);
+    areat = areas + 2*areaq;
+    System.out.println("A área total é " + areat);
   }
 }
