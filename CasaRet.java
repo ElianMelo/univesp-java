f6c15bcb85bf69b70b319e87aa84859e92624f2a
ElianMelo <elianmelomorais@gmail.com>
2018-10-02 17:16:44 -0300

    Primeiro Programa

---
+++ CasaRet.java
@@ -0,0 +1,9 @@
+/*
+  Programa para calculara área de uma casa (e seus cômodos) de 3 cômodos:
+  uma sala de 10x10m, um banheiro e um quarto  de 5x7m cada
+*/
+class CasaRet {
+  public static void main(String[] args){
+    System.out.println("Programa para cálculo \nda área da casa");
+  }
+}
