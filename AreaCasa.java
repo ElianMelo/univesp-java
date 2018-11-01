9b5d11d6996c302de517ac2b4df658c1ac5ab378
ElianMelo <elianmelomorais@gmail.com>
2018-11-01 15:07:18 -0300

    Espaçamento de 4 espaços

--- AreaCasa.java
+++ AreaCasa.java
@@ -8,69 +8,68 @@ e o valor da piscina de acordo com cada material, além do preço do metro quard
 
 Baseado no curso Programação de Computadores da  Univesp - Universidade Virtual do Estado de São Paulo.
 Professores responsáveis: Luciano Digiampietri e Norton Trevisan Roman.
-https://www.youtube.com/playlist?list=PLxI8Can9yAHfK6wdaMUO74lmotAP7J7bi 
+https://www.youtube.com/playlist?list=PLxI8Can9yAHfK6wdaMUO74lmotAP7J7bi
 */
 class AreaCasa {
-  //Variáveis globais
-  static final int alvenaria = 0;
-  static final int vinil = 1;
-  static final int fibra = 2;
-  static final int plastico = 3;
-  static double valorM2 = 1500;
+    //Variáveis globais
+    static final int alvenaria = 0;
+    static final int vinil = 1;
+    static final int fibra = 2;
+    static final int plastico = 3;
+    static double valorM2 = 1500;
 
-  //Area da casa
-  static void areaCasa(float lateral, float compQuarto){
+    //Area da casa
+    static void areaCasa(float lateral, float compQuarto){
 
     //Verifica se os valores são positivos
     if(lateral >= 0 && compQuarto >= 0){
-      float areaQuarto; // Area do quarto
-      float areaSala; // Area da sala
-      float areaTotal; // Area total
+        float areaQuarto; // Area do quarto
+        float areaSala; // Area da sala
+        float areaTotal; // Area total
 
-      System.out.println("Programa para cálculo da área da casa");
-      areaSala = lateral * lateral;
-      System.out.println("A área da sala é " + areaSala);
-      areaQuarto = compQuarto * (lateral / 2);
-      System.out.println("A área do quarto é " + areaQuarto);
-      System.out.println("A área do banheiro é " + areaQuarto);
-      areaTotal = areaSala + 2 * areaQuarto;
-      System.out.println("A área total é " + areaTotal);
+        System.out.println("Programa para cálculo da área da casa");
+        areaSala = lateral * lateral;
+        System.out.println("A área da sala é " + areaSala);
+        areaQuarto = compQuarto * (lateral / 2);
+        System.out.println("A área do quarto é " + areaQuarto);
+        System.out.println("A área do banheiro é " + areaQuarto);
+        areaTotal = areaSala + 2 * areaQuarto;
+        System.out.println("A área total é " + areaTotal);
     }
     else
-      System.out.println("Erro: parâmetro < 0");
-  }
-
-  //Valor metro quadrado
-  static double valor(double area){
-    return(area >= 0 ? valorM2 * area : -1);
-  }
-
-  //Valor metro quadrado para piscina
-  static double valorPiscina(double area, int material){
-    double valor;
-    switch(material){
-      case alvenaria: return(area * 1500);
-      case vinil: return(area * 1100);
-      case fibra: return(area * 750);
-      case plastico: return(area * 500);
-      default: return(-1);
+        System.out.println("Erro: parâmetro < 0");
     }
-  }
 
-  //Area da piscina
-  static double areaPiscina(double raio){
-    return(raio >= 0 ? (Math.PI * Math.pow(raio, 2)) : -1);
-  }
-
-  public static void main(String[] args){
-    //Aqui vão os testes
-    System.out.println("Area\tMaterial\tValor");
-
-    for(double area = 50; area <= 200; area += 50){
-      for(int tipo = alvenaria; tipo <= plastico; tipo++){
-        System.out.println(area + "\t" + tipo + "\t\t" + valorPiscina(area, tipo));
-      }
+    //Valor metro quadrado
+    static double valor(double area){
+        return(area >= 0 ? valorM2 * area : -1);
     }
-  }
 
+    //Valor metro quadrado para piscina
+    static double valorPiscina(double area, int material){
+        double valor;
+        switch(material){
+            case alvenaria: return(area * 1500);
+            case vinil: return(area * 1100);
+            case fibra: return(area * 750);
+            case plastico: return(area * 500);
+            default: return(-1);
+        }
+    }
+
+    //Area da piscina
+    static double areaPiscina(double raio){
+        return(raio >= 0 ? (Math.PI * Math.pow(raio, 2)) : -1);
+    }
+
+    public static void main(String[] args){
+        //Aqui vão os testes
+        System.out.println("Area\tMaterial\tValor");
+
+        for(double area = 50; area <= 200; area += 50){
+            for(int tipo = alvenaria; tipo <= plastico; tipo++){
+                System.out.println(area + "\t" + tipo + "\t\t" + valorPiscina(area, tipo));
+            }
+        }
+    }
 }
