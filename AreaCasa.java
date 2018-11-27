82deacaca596610fe494835880983fa2d7e078ba
ElianMelo <elianmelomorais@gmail.com>
2018-11-27 15:27:31 -0200

    POO - Separação do AreaCasa em AreaCasa e AreaPiscina e a exclusão de matrizes do código

--- AreaCasa.java
+++ AreaCasa.java
@@ -10,28 +10,13 @@ Baseado no curso Programação de Computadores da  Univesp - Universidade Virtua
 Professores responsáveis: Luciano Digiampietri e Norton Trevisan Roman.
 https://www.youtube.com/playlist?list=PLxI8Can9yAHfK6wdaMUO74lmotAP7J7bi
 */
+/* Programa para calcular a área de uma casa (e seus cômodos) de 3 cômodos:
+   uma sala, um banheiro e um quarto */
 class AreaCasa {
-    // Materias da piscina
-    static final int alvenaria = 0;
-    static final int vinil = 1;
-    static final int fibra = 2;
-    static final int plastico = 3;
-
-    // Nomes dos materiais
-    static char[][] nomes = {
-        {'A', 'l', 'v', 'e', 'n', 'a', 'r', 'i', 'a'},
-        {'V', 'i', 'n', 'i', 'l'},
-        {'F', 'i', 'b', 'r', 'a'},
-        {'P', 'l', 'a', 's', 't', 'i', 'c', 'o'}
-    };
-
-    // Preço dos materiais
-    static double[] precos = {1500, 1100, 750, 500};
-
-    // Valor do metro quadrado
+    // Valor do metro quadrado da casa
     static double valorM2 = 1500;
 
-    // Area da casa
+    // Calcula a área da casa
     static void areaCasa(float lateral, float compQuarto){
 
     // Verifica se os valores são positivos
@@ -53,118 +38,13 @@ class AreaCasa {
         System.out.println("Erro: parâmetro < 0");
     }
 
-    // Valor do metro quadrado de acordo com a área
+    // Calcula o valor total da casa
     static double valor(double area){
         return (area >= 0 ? valorM2 * area : -1);
     }
 
-    // Valor metro quadrado para piscina
-    static double valorPiscina(double area, int material){
-        if(material < alvenaria || material > plastico || area < 0)
-            return(-1);
-
-        return (area * precos[material]);
-    }
-
-    // Area da piscina
-    static double areaPiscina(double raio){
-        return (raio >= 0 ? (Math.PI * Math.pow(raio, 2)) : -1);
-    }
-
-    // Calculo da media
-    static double media(double[] arranjo){
-        double resp = 0;
-
-        for (double valor : arranjo) {
-            resp += valor;
-        }
-
-        return (resp/arranjo.length);
-    }
-
-    // Verifica se o caractere é minusculo
-    static boolean minuscula(char c){
-        return(c >= 'a' && c <= 'z');
-    }
-
-    // Carrega os valores das piscinas na matriz de área x material
-    static void carregaVal(double[][] m){
-        for (int i = 0; i < m.length; i++) { //Linhas material
-            for (int j = 50; j <= 200; j += 50) { //Colunas áreas
-                m[i][j / 50 - 1] = valorPiscina(j, i);
-            }
-        }
-    }
-    // Retorna a matriz com os preços finais.
-    // Parâmetros:
-    //     val - matriz de valores
-    //     desc - matriz de descontos
-    static double[][] calculaFinal(double[][] val, double[][] desc){
-        double[][] saida = new double[val.length][val[0].length];
-
-        for (int i = 0; i < saida.length; i++) {
-            for (int j = 0; j < saida[0].length ; j++) {
-                saida[i][j] = val[i][j] * (1 - desc[i][j]);
-            }
-        }
-
-        return (saida);
-    }
-
-    static double[][] somaMatrizes(double[][] a, double[][] b){
-        if (a.length != b.length || a[0].length != b[0].length){
-            return null;
-        } else {
-            double[][] resp = new double[a.length][a[0].length];
-            for (int i = 0; i < resp.length; i++) {
-                for (int j = 0; j < resp[0].length; j++) {
-                    resp[i][j] = a[i][j] + b[i][j];
-                }
-            }
-            return resp;
-        }
-    }
-
-    static double[][] subtraiMatrizes(double[][] a, double[][] b){
-        if (a.length != b.length || a[0].length != b[0].length){
-            return null;
-        } else {
-            double[][] resp = new double[a.length][a[0].length];
-            for (int i = 0; i < resp.length; i++) {
-                for (int j = 0; j < resp[0].length; j++) {
-                    resp[i][j] = a[i][j] - b[i][j];
-                }
-            }
-            return resp;
-        }
-    }
-
-    static double[][] transporMatriz(double[][] m){
-        double[][] trans = new double[m[0].length][m.length];
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[0].length; j++) {
-                trans[j][i] = m[i][j];
-            }
-        }
-        return trans;
-    }
-
     public static void main(String[] args){
         // Aqui vão os testes
-        double[][] a = {
-            {1, 3, -2, 3.5},
-            {2, 4, -4, 6},
-            {-2, -1.5, 3, 3}
-        };
-
-        double[][] trans = transporMatriz(a);
-
-        for (int i = 0; i < trans.length; i++) {
-            for (int j = 0; j < trans[i].length; j++) {
-                System.out.print(trans[i][j] + "\t");
-            }
-            System.out.println();
-        }
 
     }
 }
--- AreaPiscina.java
+++ AreaPiscina.java
@@ -1,12 +1,61 @@
 class AreaPiscina {
-  public static void main(String[] args){
-    double raio = 2;
-    double areap;
-    final double pi = 3.14159;
+    // Materias da piscina
+    static final int alvenaria = 0;
+    static final int vinil = 1;
+    static final int fibra = 2;
+    static final int plastico = 3;
 
-    areap = pi * raio * raio;
-    //areap = Math.PI * Math.pow(raio, 2);
-    System.out.println("área: " + areap);
-  }
+    // Preço dos materiais
+    static double[] precos = {1500, 1100, 750, 500};
 
+    // Nomes dos materiais
+    static char[][] nomes = {
+        {'A', 'l', 'v', 'e', 'n', 'a', 'r', 'i', 'a'},
+        {'V', 'i', 'n', 'i', 'l'},
+        {'F', 'i', 'b', 'r', 'a'},
+        {'P', 'l', 'a', 's', 't', 'i', 'c', 'o'}
+    };
+
+    // Calcula a área da piscina
+    static double areaPiscina(double raio){
+        return (raio >= 0 ? (Math.PI * Math.pow(raio, 2)) : -1);
+    }
+
+    // Calcula o valor do metro quadrado para piscina
+    static double valorPiscina(double area, int material){
+        if(material < alvenaria || material > plastico || area < 0)
+            return(-1);
+
+        return (area * precos[material]);
+    }
+
+    // Retorna a matriz com os preços finais.
+    // Parâmetros:
+    //     val - matriz de valores
+    //     desc - matriz de descontos
+    static double[][] calculaFinal(double[][] val, double[][] desc){
+        double[][] saida = new double[val.length][val[0].length];
+
+        for (int i = 0; i < saida.length; i++) {
+            for (int j = 0; j < saida[0].length ; j++) {
+                saida[i][j] = val[i][j] * (1 - desc[i][j]);
+            }
+        }
+
+        return (saida);
+    }
+
+    // Carrega os valores das piscinas na matriz de área x material
+    static void carregaVal(double[][] m){
+        for (int i = 0; i < m.length; i++) { //Linhas material
+            for (int j = 50; j <= 200; j += 50) { //Colunas áreas
+                m[i][j / 50 - 1] = valorPiscina(j, i);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        // Aqui vão os testes
+
+    }
 }
