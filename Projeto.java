aa2d99c18ae045997e5335254b44e4cc4b362dd9
ElianMelo <elianmelomorais@gmail.com>
2018-11-29 17:24:35 -0200

    Adiciona um array de objetos e o manipula

--- Projeto.java
+++ Projeto.java
@@ -1,4 +1,19 @@
  class Projeto {
+     Residencia[] condominio;
+     int ultimo = -1; // Último alocado
+
+     boolean adicionaRes(Residencia r){
+         if(this.ultimo < this.condominio.length-1){
+             ultimo++;
+             this.condominio[ultimo] = r;
+             return true;
+         }
+         return false;
+     }
+
+     Projeto(int tam){
+         condominio = new Residencia[tam];
+     }
 
      static double area(AreaCasa casa, AreaPiscina piscina){
          return(casa.area() + piscina.area());
@@ -6,11 +21,24 @@
 
      public static void main(String[] args) {
          // Aqui vão os testes
-         AreaCasa casaPrinc1 = new AreaCasa(10, 5);
-         AreaCasa casaPrinc2 = new AreaCasa(20, 5);
-         AreaPiscina piscina1 = new AreaPiscina(10);
+         Projeto proj = new Projeto(3);
 
-         System.out.println(area(casaPrinc1, piscina1));
+         AreaCasa c = new AreaCasa(10, 5);
+         AreaPiscina p = new AreaPiscina(5);
+         Residencia r = new Residencia(c, p);
+         proj.adicionaRes(r);
+
+         c = new AreaCasa(12, 7);
+         p = new AreaPiscina(6);
+         r = new Residencia(c, p);
+         proj.adicionaRes(r);
+
+         c = new AreaCasa(10, 6);
+         p = new AreaPiscina(3.5);
+         r = new Residencia(c, p);
+         proj.adicionaRes(r);
+
+         System.out.println(proj.condominio[1].casa.area());
      }
 
  }
