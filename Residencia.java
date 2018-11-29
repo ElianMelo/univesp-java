45ff6be885474bff6f9ed0acf47dbae75e738fc1
ElianMelo <elianmelomorais@gmail.com>
2018-11-29 15:52:17 -0200

    Cria a classe Residencia que recebe AreaCasa e AreaPiscina

---
+++ Residencia.java
@@ -0,0 +1,13 @@
+class Residencia {
+    AreaCasa casa;
+    AreaPiscina piscina;
+
+    Residencia(AreaCasa casa, AreaPiscina piscina){
+        this.casa = casa;
+        this.piscina = piscina;
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
