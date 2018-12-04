9e9eb2661eadf0cdc19ebd4c45221c01b987fd2f
ElianMelo <elianmelomorais@gmail.com>
2018-12-04 17:13:01 -0200

    Adiciona busca binaria e comentarios

--- Projeto.java
+++ Projeto.java
@@ -2,6 +2,7 @@
      Residencia[] condominio;
      int ultimo = -1; // Último alocado
 
+     // Método que adiciona residencias ao condominio
      boolean adicionaRes(Residencia r){
          if(this.ultimo < this.condominio.length-1){
              ultimo++;
@@ -11,14 +12,17 @@
          return false;
      }
 
+     // Construtor de um projeto
      Projeto(int tam){
          condominio = new Residencia[tam];
      }
 
+     // Calcula a area do conjunto casa + piscina (residência)
      static double area(AreaCasa casa, AreaPiscina piscina){
          return(casa.area() + piscina.area());
      }
 
+     // Faz uma busca sequencial
      static int buscaSeq(int[] arr, int el){
          for(int i = 0; i < arr.length; i++){
              if(arr[i] == el) return(i);
@@ -27,30 +31,43 @@
          return(-1);
      }
 
+     // Faz uma busca sequencial pelo raio das piscinas
      int buscaPiscSeq(double raio){
          for(int i = 0; i < this.condominio.length; i++)
              if(this.condominio[i].piscina.raio == raio) return(i);
          return(-1);
      }
 
+     // Faz uma busca binária
+     static int buscaBin(int[] arr, int el){
+         int fim = arr.length-1;
+         int ini = 0;
+         while(ini <= fim){
+             int meio = (fim + ini) / 2;
+             if(arr[meio] < el) ini = meio + 1;
+             else
+                if(arr[meio] > el) fim = meio - 1;
+                else return(meio);
+         }
+         return(-1);
+     }
+
+     // Faz uma busca binária nas áreas das casas do condominio
+     static int buscaBin(Residencia[] arr, double area){
+         int fim = arr.length-1;
+         int ini = 0;
+         while(ini <= fim){
+             int meio = (fim + ini) / 2;
+             if(arr[meio].casa.area() < area) ini = meio + 1;
+             else
+                if(arr[meio].casa.area() > area) fim = meio - 1;
+                else return(meio);
+         }
+         return(-1);
+     }
+
      public static void main(String[] args) {
          // Aqui vão os testes
-         int[] v = {-78, -4, 0, 32, 52, 55, 63, 69, 125, 200};
-
-         System.out.println(buscaSeq(v, 63));
-         System.out.println(buscaSeq(v, 68));
-
-         Projeto pr = new Projeto(5);
-
-         for(int i = 0; i < pr.condominio.length; i++){
-             AreaCasa c = new AreaCasa();
-             AreaPiscina p = new AreaPiscina(i+2);
-             Residencia r = new Residencia(c, p);
-             pr.adicionaRes(r);
-         }
-
-         System.out.println(pr.buscaPiscSeq(3));
-         System.out.println(pr.buscaPiscSeq(15));
      }
 
  }
--- Residencia.java
+++ Residencia.java
@@ -2,12 +2,13 @@ class Residencia {
     AreaCasa casa;
     AreaPiscina piscina;
 
+    // Construtor de uma residência
     Residencia(AreaCasa casa, AreaPiscina piscina){
         this.casa = casa;
         this.piscina = piscina;
     }
 
     public static void main(String[] args){
-        
+        //Aqui vão os testes
     }
 }
