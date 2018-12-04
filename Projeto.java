17a25211fa1e75f8b74c4342cae28d69a72a43f8
ElianMelo <elianmelomorais@gmail.com>
2018-12-04 15:07:35 -0200

    Adiciona contexto e exemplos a busca sequencial

--- Projeto.java
+++ Projeto.java
@@ -20,17 +20,37 @@
      }
 
      static int buscaSeq(int[] arr, int el){
-         for(int i = 0; i < arr.length; i++)
+         for(int i = 0; i < arr.length; i++){
              if(arr[i] == el) return(i);
+             if(arr[i] > el) break;
+         }
+         return(-1);
+     }
+
+     int buscaPiscSeq(double raio){
+         for(int i = 0; i < this.condominio.length; i++)
+             if(this.condominio[i].piscina.raio == raio) return(i);
          return(-1);
      }
 
      public static void main(String[] args) {
          // Aqui vão os testes
-         int[] v = {9, 8, 4, 6, 3, 4};
+         int[] v = {-78, -4, 0, 32, 52, 55, 63, 69, 125, 200};
 
-         System.out.println(buscaSeq(v, 4));
-         System.out.println(buscaSeq(v, 12));
+         System.out.println(buscaSeq(v, 63));
+         System.out.println(buscaSeq(v, 68));
+
+         Projeto pr = new Projeto(5);
+
+         for(int i = 0; i < pr.condominio.length; i++){
+             AreaCasa c = new AreaCasa();
+             AreaPiscina p = new AreaPiscina(i+2);
+             Residencia r = new Residencia(c, p);
+             pr.adicionaRes(r);
+         }
+
+         System.out.println(pr.buscaPiscSeq(3));
+         System.out.println(pr.buscaPiscSeq(15));
      }
 
  }
